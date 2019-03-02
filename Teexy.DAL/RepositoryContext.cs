7d76d0d66464888ccdc848e566abf075ace4dd15
Palindromer <ole-koo@yandex.ua>
2019-03-03 00:02:46 +0200

    Commit

--- Teexy.DAL/RepositoryContext.cs
+++ Teexy.DAL/RepositoryContext.cs
@@ -31,7 +31,8 @@ namespace Teexy.DAL
 		{
 			builder.EnableSensitiveDataLogging(true);
 			//builder.UseMySql("server=localhost;port=3306;UserId=rooter;Password=rooter;database=teexy;");
-			builder.UseMySql("server=palmysqlserver.mysql.database.azure.com; Port=3306; Database=Teexy; Uid=Palindromer@palmysqlserver; Pwd=jfrpz(*#5; SslMode=Preferred;");
+			builder.UseMySql("server=91.227.183.147;port=8306;UserId=teexy-user;Password=teexy-user;database=teexy;");
+			//builder.UseMySql("server=palmysqlserver.mysql.database.azure.com; Port=3306; Database=Teexy; Uid=Palindromer@palmysqlserver; Pwd=jfrpz(*#5; SslMode=Preferred;");
 		}
 	}
 }
--- Teexy/Startup.cs
+++ Teexy/Startup.cs
@@ -48,9 +48,11 @@ namespace Teexy
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			app.UseDeveloperExceptionPage();
+
 			if (env.IsDevelopment())
 			{
-				app.UseDeveloperExceptionPage();
+
 			}
 			else
 			{
